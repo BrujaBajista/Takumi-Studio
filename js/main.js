f75eab27b6815add48bde1c99915e1d5637851cb
Zaida Alvarez Quintana <zaidaalvarezquintana@gmail.com>
2023-08-31 20:17:51 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -12,9 +12,41 @@ const contactanos2 = () => {
 const contactanos3 = () => {
     alert(datos_contacto3);
 };
-do {
-    opcion = parseInt(prompt("Solicite su Consulta\n\n1. Solicitar un Presupuesto\n2. Asesoramiento de Enmarcación\n3. Consultas sobre su Compra\n4. Contactarse con Nosotros\n\nPARA SEGUIR NAVEGANDO LA PAGINA INGRESE 0"));
 
+const productos = [
+    { id: 1, nombre: "Cuadro A3 (30x45)", precio: 10000 },
+    { id: 2, nombre: "Cuadro A4 (21x30cm)", precio: 8000 },
+    { id: 3, nombre: "Cuadro A5 (15x25cm)", precio: 6000 },
+    { id: 4, nombre: "Cuadro Porta Retrato (13x18cm)", precio: 4000 },
+    { id: 5, nombre: "Ventana de Passepartou", precio: 2000 },
+    { id: 6, nombre: "Espejo Personalizado", precio: 0 },
+    { id: 7, nombre: "Cuadro Personalizado", precio: 0 },
+];
+
+const carrito = [];
+
+function agregar_carrito(id) {
+    const producto_encontrado = productos.find((producto) => producto.id === id);
+    if (producto_encontrado) {
+        carrito.push(producto_encontrado);
+        alert(`Producto "${producto_encontrado.nombre}" agregado al carrito.`);
+    } else {
+        alert("Producto Fuera de Stock.");
+    }
+}
+
+function mostrar_carrito() {
+    let carrito_info = "Productos en el carrito:\n\n";
+    carrito.forEach((producto) => {
+        carrito_info += `${producto.nombre} - Precio: ${producto.precio}\n`;
+    });
+    const carrito_total = carrito.reduce((total, producto) => total + producto.precio, 0);
+    carrito_info += `\nPrecio total del carrito: ${carrito_total}`;
+    alert(carrito_info);
+}
+
+do {
+    opcion = parseInt(prompt("Solicite su Consulta\n\n1. Solicitar un Presupuesto\n2. Asesoramiento de Enmarcación\n3. Consultas sobre su Compra\n4. Contactarse con Nosotros\n5. Ver Productos y Carrito\n\nPARA SEGUIR NAVEGANDO LA PAGINA INGRESE 0"));
     switch (opcion) {
         case 0:
             alert("¡Que Disfrutes la Página!");
@@ -25,7 +57,7 @@ do {
             let consulta;
             let mail;
             do {
-                medidas = prompt("Ingrese las Medidas de su Obra en Cm (Ej: 17cmx37)");
+                medidas = prompt("Ingrese las Medidas de su Obra en Centimetros (Ej: 17cm x 37cm)");
                 consulta = prompt("Ingrese su Consulta");
                 mail = prompt("Ingrese su Correo Electrónico");
                 alert("Gracias por su Consulta y que Disfrutes la Página");
@@ -41,8 +73,20 @@ do {
         case 4:
             contactanos3();
             break;
+        case 5:
+            while (true) {
+                const seleccion = parseInt(prompt("Seleccione sus Productos\n\n1. Cuadro A3 (30x45)\n2. Cuadro A4 (21x30cm)\n3. Cuadro A5 (15x25cm)\n4. Cuadro Porta Retrato (13x18cm)\n5. Ventana de Passepartou\n6. Espejo Personalizado\n7. Cuadro Personalizado\n\nPARA FINALIZAR EL CARRITO INGRESE 0"));
+                if (seleccion === 0) {
+                    break;
+                }
+                agregar_carrito(seleccion);
+            }
+            mostrar_carrito();
+            break;
         default:
-            alert("Opción NO Válida");
+            if (!isNaN(opcion)) {
+                alert("Opción NO Válida");
+            }
             break;
     }
-} while (!terminar_bucle); 
+} while (!terminar_bucle);
